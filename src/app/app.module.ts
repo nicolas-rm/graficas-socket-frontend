9726c59383c2fdff884370a5bcd6a4d2ca7f79d2
Nicolás Rincón Mújica <nicolas.rm540@gmail.com>
2020-08-12 21:42:34 -0500

    Configuracion e inicializacion principal de frontend de graficas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// Importaciones de sockets
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { environment } from 'src/environments/environment';
+
+// Configuracion del puerto socket, donde escuchara los eventos y conexiones.
+const config: SocketIoConfig = { url: environment.wsPort, options: {} };
+
+// Importaciones para las graficas
+import { ChartsModule } from 'ng2-charts';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
